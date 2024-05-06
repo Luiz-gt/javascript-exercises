4ad10f8c39ddd249d5d06c514c5de9c894d2f7e8
luiz.gt <luiz.gt@hotmail.com>
2024-05-06 09:44:51 -0300

    Resolução dos exercicios 1, 2 e 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+// Declarando variáveis
+let stringConc='';   
+let i=1;
 
+
+if(num<0){
+
+    stringConc='ERROR'; //testando números negativos
+}
+else {
+    while(i<=num){
+
+        stringConc=stringConc+string; //concatenando a string
+        i++;
+    }
+}
+return(stringConc)
 };
 
 // Do not edit below this line
