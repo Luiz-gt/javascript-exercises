6d77e121d1abc9301393478b217c7580adfa7e13
luiz.gt <luiz.gt@hotmail.com>
2024-05-08 09:43:57 -0300

    exercicio 5 completado

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(A,...B) {          
+i=0;
+while(i<B.length){
+    
+    if (A.includes(B[i])){
+        A.splice(A.indexOf(B[i]),1);
+    } 
+    else{
+        i++;
+    }
+}
+return(A);
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
 
+//testando se os parametros são positivos
+if (int1<0||int2<0){
+    return('ERROR');
+}
+//testando se os parametros são números
+else if(typeof int1!='number'||typeof int2!='number'){
+    return('ERROR');
+}
+
+//Se os parametros passarem, começo da operação
+else{
+
+let s=0;
+let i = 0;
+
+if (int1<int2){
+   while(i<=int2-int1) {
+    s=s+int1+i;
+    i++;
+   }
+}
+else{
+    while(i<=int1-int2){
+        s=s+int2+i;
+        i++;
+    }
+}
+return(s);
+}
 };
 
 // Do not edit below this line
