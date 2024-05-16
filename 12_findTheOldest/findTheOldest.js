008e2a7edb8bc66e4d973451cca3c9e8bf34131b
luiz.gt <luiz.gt@hotmail.com>
2024-05-16 15:12:39 -0300

    12 exercício completado

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+let age=people.map((obj)=>{
 
+    if('yearOfDeath' in obj){
+        return obj.yearOfDeath-obj.yearOfBirth;}
+
+    else{
+        let d=new Date();
+        let year=d.getFullYear();
+        return year-obj.yearOfBirth;}
+}
+);
+
+let oldest=age[0];
+let ind=0;
+
+for (let i=1;i<age.length;i++) { 
+    
+    if (age[i]>oldest){
+        oldest=age[i];
+        ind=i;
+    }
+};
+
+return (people[ind]);
 };
 
 // Do not edit below this line
