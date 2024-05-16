d0cb0746515f1184169e0fc2fc8b41a4c44f3734
luiz.gt <luiz.gt@hotmail.com>
2024-05-16 14:16:07 -0300

    Exercicios 10 e 11 completados

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+// Defininindo condições iniciais
+if(x!='number')x=parseInt(x);
+if(x<0||typeof x!='number')return('OOPS');
+if(x===0)return(0);
+if(x===1)return(1);
+if(x===2)return(1);
+
+// Começo do código de verdade
+const F=Array(x);
+
+i=2;
+F[0]=1;
+F[1]=1;
+
+while(i<=x-1){
+F[i]=F[i-1]+F[i-2];
+i++;
+}
+
+return(F[x-1]);
 };
 
 // Do not edit below this line
